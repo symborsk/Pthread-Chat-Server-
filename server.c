42ed3f9b70119790aacb306b76b18190d845da3a
Aaron Philips <aphilips@cs.ualberta.ca>
2016-10-26 21:35:05 -0600

    server side after longnight2

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/time.h>
 #define	MY_PORT	2222
 #define MAXSIZE 20
 /* ---------------------------------------------------------------------
@@ -15,17 +16,27 @@
  distinct numbers. The server and the clients may run on different ma-
  chines.
  --------------------------------------------------------------------- */
-typedef struct usernames{
+typedef struct users{
 	int length;
 	unsigned char usernamestr [MAXSIZE];
-} username;
+	pid_t pid;
+} user;
+
 int main()
-{
+{	user listofuser[MAXSIZE];
+	fd_set fd_active;
+	fd_set read_fd_set;
+	struct timeval timer;
+	timer.tv_sec=5;
+	timer.tv_usec=2000;
+	
+		
+
 	int	sock, snew, fromlength, number, outnum;
 	uint16_t inlength,usernameLength;
 	uint32_t inusername;
 	struct	sockaddr_in	master, from;
-
+	
 
 	sock = socket (AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
@@ -42,51 +53,103 @@ int main()
 		exit (1);
 	}
 
-	number = 0;
-
 	listen (sock, 5);
+	FD_ZERO(&fd_active);
+	FD_SET(sock,&fd_active);
 
-	
-	fromlength = sizeof (from);
-	snew = accept (sock, (struct sockaddr*) & from, & fromlength);
-	if (snew < 0) {
-		perror ("Server: accept failed");
-		exit (1);
-	}
-	number=0xcf;
-	outnum = htonl (number);
-	send (snew, &outnum, sizeof (outnum),0);
-
-	number=0xa7;
-	outnum = htonl (number);
-	send (snew, &outnum, sizeof (outnum),0);
-	// int myint=0;
-	// int tmp=0;
-	// read(snew,&tmp,sizeof(tmp));
-	// myint=ntohl(tmp);
-	// printf("%d",myint);
-
-	inlength=0;
-	int err=recv(snew,&inlength,sizeof(inlength),0);
-	printf("%d %d\n",err,errno);
-	printf("%u\n",inlength);
-	usernameLength=ntohs(inlength);
-	printf("%d\n",usernameLength);
-
-	unsigned char usernamestr [usernameLength];
-	recv(snew,&usernamestr,sizeof(usernamestr),0);
-	
-	
-	// uint32_t unCodedName = ntohl(inusername);
-    // printf("%u\n",unCodedName);
-	// memcpy(username, (unsigned char*)&unCodedName, sizeof(unCodedName));
-	printf("This: %s  is the username",usernamestr);
-	username usernames [MAXSIZE];
-	int i;
-	for(i=0;i<MAXSIZE;i++){
+	for(;;){
 		
-	}
-	close (snew);
+
+		number = 0;
+		user currentuser=listofuser[0];
+
+		read_fd_set=fd_active;
+		printf("got past select\n");
+	
+		if(select(FD_SETSIZE,&read_fd_set,NULL,NULL,NULL)<0){
+			
+			perror("timeout123");
+			exit(1);
+		}
 		
+		// pid_t forkstatus=fork();
+		// if(forkstatus==0)
+		// {
+		int i=0;
+		for(i;i<FD_SETSIZE;i++){			
+			if(FD_ISSET(i, &read_fd_set)){
+				if(i == sock){
+					fromlength = sizeof (from);
+					printf("this is snew: %d\n",snew);
+					snew = accept (sock, (struct sockaddr*) & from, & fromlength);
+					if(setsockopt(snew,SOL_SOCKET,SO_RCVTIMEO, (char *)&timer,sizeof(timer))<0){
+						perror("setting timeout failed");
+					}
+					if (snew < 0) 
+					{
+						perror ("Server: accept failed");
+						exit (1);
+					}
+					number=0xcf;
+					outnum = htonl (number);
+					send (snew, &outnum, sizeof (outnum),0);
+
+					number=0xa7;
+					outnum = htonl (number);
+					send (snew, &outnum, sizeof (outnum),0);
+					
+					FD_SET(snew, &fd_active);
+					// int myint=0;
+					// int tmp=0;
+					// read(snew,&tmp,sizeof(tmp));
+					// myint=ntohl(tmp);
+					// printf("%d",myint);
+
+					inlength=-1;
+					int a = recv(snew,&inlength,sizeof(inlength),0);
+					printf(" a is %d\n", a);
+
+					if(a  ==  -1){
+						
+						printf("closing\n");
+						uint16_t exit_val = -1;
+						//uint16_t outexit=htons(exit_val);
+						send(snew,&exit_val,sizeof(exit_val),0); 
+						//shutdown(snew,SHUT_RDWR);	
+						close (snew);	
+					}
+					else{
+						usernameLength=ntohs(inlength);
+						printf("%d\n",usernameLength);
+
+						unsigned char usernamestr [usernameLength];
+						recv(snew,&usernamestr,sizeof(usernamestr),0);
+
+						// uint32_t unCodedName = ntohl(inusername);
+					    // printf("%u\n",unCodedName);
+						// memcpy(username, (unsigned char*)&unCodedName, sizeof(unCodedName));
+						printf("This: %s  is the username",usernamestr);
+						currentuser.length=usernameLength;
+						strcpy(currentuser.usernamestr,usernamestr);
+						//currentuser.usernamestr=usernamestr;
+						currentuser.pid=0;
+						printf("%s\n",currentuser.usernamestr );
+						close (snew);
+					}
+					
+				}
+			}
+			else
+			{
+				FD_CLR(i,&fd_active);
+			}
+		}
+		// }
+		// else
+		// {
+
+		// }
+
+	}	
 	
 }
