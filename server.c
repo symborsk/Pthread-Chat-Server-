61d70df2b373e372359aec5eaf1c0c109c11998c
aphilips@ualberta.ca <aphilips@cs.ualberta.ca>
2016-10-28 17:49:37 -0600

    can send records server side commit

--- server.c
+++ server.c
@@ -9,6 +9,7 @@
 #include <sys/time.h>
 #define	MY_PORT	2222
 #define MAXSIZE 20
+
 /* ---------------------------------------------------------------------
  This	is  a sample server which opens a stream socket and then awaits
  requests coming from client processes. In response for a request, the
@@ -16,6 +17,10 @@
  distinct numbers. The server and the clients may run on different ma-
  chines.
  --------------------------------------------------------------------- */
+void handShake(int snew);
+uint16_t lengthOfUsername(unsigned char userName[MAXSIZE]);
+void sendCurrentUserNames(int snew);
+
 typedef struct users{
 	int length;
 	unsigned char usernamestr [MAXSIZE];
@@ -24,23 +29,20 @@ typedef struct users{
 
 int number = 0;
 int numberofclients=0;
-
-int main()
-{	user listofuser[MAXSIZE];
+user listofuser[MAXSIZE];
+int main(){	
+	
 	fd_set fd_active;
 	fd_set read_fd_set;
 	struct timeval timer;
 	timer.tv_sec=10;
 	timer.tv_usec=2000;
 	
-		
-
 	int	sock, snew, fromlength, number, outnum;
 	uint16_t inlength,usernameLength, in_uint16_t;
 	uint32_t inusername;
 	struct	sockaddr_in	master, from;
 	
-
 	sock = socket (AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
 		perror ("Server: cannot open master socket");
@@ -60,26 +62,26 @@ int main()
 	FD_ZERO(&fd_active);
 	FD_SET(sock,&fd_active);
 
+	pid_t forkstatus = 1; 
 	for(;;){
+		number = 0;
 		
 
-		number = 0;
-		user currentuser=listofuser[0];
-
 		read_fd_set=fd_active;
-	
+
+		printf("hitting select\n");
 		if(select(FD_SETSIZE,&read_fd_set,NULL,NULL,NULL)<0){
 			
-			perror("timeout123");
+			printf("forkstatus %d", forkstatus);
+			perror("select()");
 			exit(1);
 		}
 		
 		numberofclients++;
-		printf("forking\n");
-		pid_t forkstatus=fork();
 
-		if(forkstatus==0)
-		{
+		//Only want to frok if we are at the parent
+		printf("%d\n", forkstatus);
+
 		int i=0;
 		for(i;i<FD_SETSIZE;i++){			
 			if(FD_ISSET(i, &read_fd_set)){
@@ -87,74 +89,126 @@ int main()
 					fromlength = sizeof (from);
 					printf("this is snew: %d\n",snew);
 					snew = accept (sock, (struct sockaddr*) & from, & fromlength);
-					if(setsockopt(snew,SOL_SOCKET,SO_RCVTIMEO, (char *)&timer,sizeof(timer))<0){
-						perror("setting timeout failed");
-					}
-					if (snew < 0) 
-					{
-						perror ("Server: accept failed");
-						exit (1);
-					}
-					number=0xcf;
-					outnum = htonl (number);
-					send (snew, &outnum, sizeof (outnum),0);
 
-					number=0xa7;
-					outnum = htonl (number);
-					send (snew, &outnum, sizeof (outnum),0);
-					
-					FD_SET(snew, &fd_active);
-					// int myint=0;
-					// int tmp=0;
-					// read(snew,&tmp,sizeof(tmp));
-					// myint=ntohl(tmp);
-					// printf("%d",myint);
+					printf("forking\n");
+					forkstatus = fork();
 
-					in_uint16_t=-1;
-					int a = recv(snew,&inlength,sizeof(inlength),0);
-					printf(" a is %d\n", a);
 
-					if(a  ==  -1){
+					//Child process
+					if(forkstatus == 0){
+						user currentuser=listofuser[numberofclients];
+						numberofclients++;
 						
-						printf("closing\n");
-						uint16_t exit_val = -1;
-						//uint16_t outexit=htons(exit_val);
-						send(snew,&exit_val,sizeof(exit_val),0); 
-						//shutdown(snew,SHUT_RDWR);	
-						close (snew);	
+						close(sock);
+						printf("Socket Accepted");
+						
+						if(setsockopt(snew,SOL_SOCKET,SO_RCVTIMEO, (char *)&timer,sizeof(timer))<0){
+							perror("setting timeout failed");
+						}
+						if (snew < 0) 
+						{
+							perror ("Server: accept failed");
+							exit (1);
+						}
+						
+						handShake(snew);
+						sendCurrentUserNames(snew);
+						FD_SET(snew, &fd_active);
+
+						in_uint16_t=-1;
+						int a = recv(snew,&inlength,sizeof(inlength),0);
+						printf(" a is %d\n", a);
+
+						if(a  ==  -1){
+							
+							printf("closing\n");
+							uint16_t exit_val = -1;
+							//uint16_t outexit=htons(exit_val);
+							send(snew,&exit_val,sizeof(exit_val),0); 
+							//shutdown(snew,SHUT_RDWR);	
+							close (snew);	
+						}
+						else{
+							usernameLength=ntohs(inlength);
+							printf("%d\n",usernameLength);
+
+							unsigned char usernamestr [usernameLength];
+							recv(snew,&usernamestr,sizeof(usernamestr),0);
+
+							//printf("This: %s  is the username",usernamestr);
+							currentuser.length=usernameLength;
+							strcpy(currentuser.usernamestr,usernamestr);
+							currentuser.pid=0;
+
+
+							close(snew);
+							exit(1);
+						}
+						
 					}
+					//Parent process
 					else{
-						usernameLength=ntohs(inlength);
-						printf("%d\n",usernameLength);
-
-						unsigned char usernamestr [usernameLength];
-						recv(snew,&usernamestr,sizeof(usernamestr),0);
-
-						// uint32_t unCodedName = ntohl(inusername);
-					    // printf("%u\n",unCodedName);
-						// memcpy(username, (unsigned char*)&unCodedName, sizeof(unCodedName));
-						printf("This: %s  is the username",usernamestr);
-						currentuser.length=usernameLength;
-						strcpy(currentuser.usernamestr,usernamestr);
-						//currentuser.usernamestr=usernamestr;
-						currentuser.pid=0;
-						printf("%s\n",currentuser.usernamestr );
-						close (snew);
+						close(snew);	
 					}
-					
+				}
+				
+				else
+				{
+					FD_CLR(i,&fd_active);
 				}
 			}
-			else
-			{
-				FD_CLR(i,&fd_active);
-			}
-		}
-		}
-		else
-		{
 
-		}
-
-	}	
+		}	
 	
+	}
 }
+
+void handShake(int snew){
+ 	uint16_t outnum=0;
+
+ 	unsigned char outarray [4];
+ 	memset(outarray,'\0',sizeof(unsigned char)*4);
+
+	uint16_t holdingNumber=0xcf;
+	outnum = htons (holdingNumber);
+	memcpy(&outarray[0], (void*) &outnum, sizeof(outnum));
+
+	holdingNumber=0xa7;
+	outnum = htons (holdingNumber);
+	memcpy(&outarray[2], (void*) &outnum, sizeof(outnum));
+	
+	printf("%u\n",outarray[0]);
+	printf("%u\n",outarray[1]);
+	printf("%u\n",outarray[2]);
+	printf("%u\n",outarray[3]);
+	send (snew, &outarray, sizeof (outarray),0);
+	perror("handShake failed");
+
+	
+}	
+
+void sendCurrentUserNames(int snew){
+	int i;
+
+	for(i=0;i<numberofclients;i++){
+		user currentuser= listofuser[i];
+		uint16_t length = htons( currentuser.length);
+		unsigned char username [currentuser.length];
+		
+		//send(snew,)
+
+	}
+
+}
+uint16_t lengthOfUsername(unsigned char userName[MAXSIZE]){
+	uint16_t i;
+	for(i = 0; i < MAXSIZE; i++){
+
+		
+		if(userName[i] == '\0')
+			break;
+	}
+
+	return i + 1;
+}
+
